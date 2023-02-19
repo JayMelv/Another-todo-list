f003a7580aecafeb37055d20d63d33f2b24a1016
JayMelv <125630015+JayMelv@users.noreply.github.com>
2023-02-19 22:05:23 +0800

    Much better code

--- main.js
+++ main.js
@@ -3,45 +3,36 @@ const listInput = document.querySelector("#user-input");
 const ul = document.querySelector("ul");
 const listItems = document.getElementsByTagName("li");
 
-//add listitem by clicking button
+function appendListItem(value) {
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(value));
+  ul.appendChild(li);
+  listInput.value = "";
+
+  let btn = document.createElement("button");
+  btn.appendChild(document.createTextNode("Delete!"));
+
+  btn.onclick = function (event) {
+    ul.removeChild(li);
+
+    event.preventDefault();
+    event.stopPropagation();
+  };
+  li.appendChild(btn);
+}
+
 submit.addEventListener("click", function () {
-  //Input needs to have a value - therefore as long as some input then append
   if (listInput.value.length > 0) {
-    //create a li element
-    let li = document.createElement("li");
-    //add text to li element
-    li.appendChild(document.createTextNode(listInput.value));
-    //add it to bottom of list
-    ul.appendChild(li);
-    //After adding reset the input
-    listInput.value = "";
-    //adds a delete button
-    let btn = document.createElement("button");
-    btn.appendChild(document.createTextNode("Delete!"));
-    ul.appendChild(btn);
+    appendListItem(listInput.value);
   }
 });
 
-//add listitem by pressing enter - event is the key pressed event
 listInput.addEventListener("keypress", function (event) {
-  //When there is input and enter is pressed
   if (listInput.value.length > 0 && event.keyCode === 13) {
-    //create a li element
-    let li = document.createElement("li");
-    //add text to li element
-    li.appendChild(document.createTextNode(listInput.value));
-    //add it to bottom of list - ul is the parent and adding list
-    ul.appendChild(li);
-    //After adding reset the input
-    listInput.value = "";
-    //adds a delete button
-    let btn = document.createElement("button");
-    btn.appendChild(document.createTextNode("Delete!"));
-    ul.appendChild(btn);
+    appendListItem(listInput.value);
   }
 });
 
-//uses onclick method to toggle strikethrough
 ul.onclick = function (event) {
   var target = event.target;
   target.classList.toggle("done");
